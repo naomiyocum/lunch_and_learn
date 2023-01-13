7d9bccf547984592089939fa2c59c93bbd8e149b
Naomi Yocum <naomiyocum24@gmail.com>
2023-01-13 15:39:12 -0700

    Add CountriesService

---
+++ spec/services/countries_service_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe CountriesService do
+  describe 'class methods' do
+    describe '.conn' do
+      it 'connects to the REST Countries API' do
+        conn = CountriesService.conn
+        expect(conn.headers).to have_value 'Faraday v2.7.2'
+      end
+    end
+
+    describe '.get_all_countries' do
+      it 'returns a collection of countries' do
+        countries = CountriesService.get_all_countries
+        expect(countries).to be_an Array
+      end
+    end
+  end
+end
